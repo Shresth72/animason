69c1ee8a09ee2e06e2c00817cd244b9bff6e6106
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-11-06 02:36:53 +0530

    adding ffmpeg

--- plug.c
+++ plug.c
@@ -1,40 +1,23 @@
 #include <assert.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
+// #include "ffmpeg/ffmpeg.h"
 #include "nob.h"
 #include "raylib/raylib-5.0_linux_amd64/include/raylib.h"
 #include "raylib/raylib-5.0_linux_amd64/include/raymath.h"
 
+#define FONT_SIZE 52
+#define CELL_WIDTH 100.0f
+#define CELL_HEIGHT 100.0f
+#define CELL_PAD (CELL_WIDTH * 0.15f)
+
+// ANIMATION
 typedef struct {
   size_t i;
   float duration;
-  bool loop;
 } Animation;
-
-typedef struct {
-  Color background;
-  Animation a;
-} Plug;
-
-static Plug *p = NULL;
-
-void plug_init(void) {
-  p = malloc(sizeof(*p));
-  assert(p != NULL);
-
-  memset(p, 0, sizeof(*p));
-  p->background = GREEN;
-  p->a.loop = true;
-
-  TraceLog(LOG_INFO, "\033[0;32m-------------------------------\033[0m");
-  TraceLog(LOG_INFO, "\033[0;32mInitialized plugin\033[0m");
-  TraceLog(LOG_INFO, "\033[0;32m-------------------------------\033[0m");
-}
-
-void *plug_pre_reload(void) { return p; }
-
-void plug_post_reload(void *state) { p = state; }
 
 typedef struct {
   float from;
@@ -42,90 +25,144 @@ typedef struct {
   float duration;
 } Keyframe;
 
-typedef struct {
-  Keyframe *items;
-  size_t count;
-  size_t capacity;
-} Keyframes;
-
 float animation_value(Animation a, Keyframe *kfs, size_t kfs_count) {
   assert(kfs_count > 0);
 
-  if (a.i >= kfs_count) {
-    return kfs[kfs_count - 1].to;
-  }
-
-  Keyframe *kf = &kfs[a.i];
-  return Lerp(kf->from, kf->to, a.duration / kf->duration);
+  Keyframe *kf = &kfs[a.i % kfs_count];
+  float t = a.duration / kf->duration;
+  t = (sinf(PI * t - PI * 0.5) + 1) * 0.5;
+  return Lerp(kf->from, kf->to, t);
 }
 
-void animation_update(Animation *a, Keyframe *kfs, size_t kfs_count) {
+void animation_update(Animation *a, float dt, Keyframe *kfs, size_t kfs_count) {
   assert(kfs_count > 0);
 
-  if (a->i >= kfs_count) {
-    if (a->loop) {
-      a->i = 0;
-      a->duration = 0;
-    } else {
-      return;
-    }
-  }
-
-  float dt = GetFrameTime();
+  a->i = a->i % kfs_count;
   a->duration += dt;
 
-  while (a->i < kfs_count && a->duration >= kfs[a->i].duration) {
+  while (a->duration >= kfs[a->i].duration) {
     a->duration -= kfs[a->i].duration;
-    a->i += 1;
+    a->i += (a->i + 1) % kfs_count;
   }
 }
 
-void plug_update(void) {
+// PLUGIN
+typedef struct {
+  Animation a;
+  Font font;
+
+} Plug;
+
+static Plug *p = NULL;
+
+static void load_resources(void) {
+  p->font = LoadFontEx(
+      "/home/shrestha/.fonts/RobotoMono/Roboto Mono Medium for Powerline.ttf",
+      FONT_SIZE, NULL, 0);
+}
+
+static void unload_resources(void) { UnloadFont(p->font); }
+
+void plug_init(void) {
+  p = malloc(sizeof(*p));
+  assert(p != NULL);
+  memset(p, 0, sizeof(*p));
+
+  load_resources();
+  TraceLog(LOG_INFO, "\033[0;32m-------------------------------\033[0m");
+  TraceLog(LOG_INFO, "\033[0;32mInitialized plugin\033[0m");
+  TraceLog(LOG_INFO, "\033[0;32m-------------------------------\033[0m");
+}
+
+void *plug_pre_reload(void) {
+  unload_resources();
+  return p;
+}
+
+void plug_post_reload(void *state) {
+  p = state;
+  load_resources();
+}
+
+void turing_machine_tape(Animation *a, float dt, float w, float h) {
+  size_t offset = 7;
   Keyframe kfs[] = {
-      {
-          .from = 0.0,
-          .to = 0.250,
-          .duration = 0.250,
-      },
-      {
-          .from = 0.25,
-          .to = 0.25,
-          .duration = 0.5,
-      },
-      {
-          .from = 0.25,
-          .to = 1.0,
-          .duration = 0.5,
-      },
+      {.from = w / 2 - CELL_WIDTH / 2 - (offset + 0) * (CELL_WIDTH + CELL_PAD),
+       .to = w / 2 - CELL_WIDTH / 2 - (offset + 0) * (CELL_WIDTH + CELL_PAD),
+       .duration = 0.5},
+      {.from = w / 2 - CELL_WIDTH / 2 - (offset + 0) * (CELL_WIDTH + CELL_PAD),
+       .to = w / 2 - CELL_WIDTH / 2 - (offset + 1) * (CELL_WIDTH + CELL_PAD),
+       .duration = 0.5},
+      {.from = w / 2 - CELL_WIDTH / 2 - (offset + 1) * (CELL_WIDTH + CELL_PAD),
+       .to = w / 2 - CELL_WIDTH / 2 - (offset + 1) * (CELL_WIDTH + CELL_PAD),
+       .duration = 0.5},
+      {.from = w / 2 - CELL_WIDTH / 2 - (offset + 2) * (CELL_WIDTH + CELL_PAD),
+       .to = w / 2 - CELL_WIDTH / 2 - (offset + 2) * (CELL_WIDTH + CELL_PAD),
+       .duration = 0.5},
+      {.from = w / 2 - CELL_WIDTH / 2 - (offset + 2) * (CELL_WIDTH + CELL_PAD),
+       .to = w / 2 - CELL_WIDTH / 2 - (offset + 2) * (CELL_WIDTH + CELL_PAD),
+       .duration = 0.5},
+      {.from = w / 2 - CELL_WIDTH / 2 - (offset + 3) * (CELL_WIDTH + CELL_PAD),
+       .to = w / 2 - CELL_WIDTH / 2 - (offset + 3) * (CELL_WIDTH + CELL_PAD),
+       .duration = 0.5},
+      // {.from = w / 2 - CELL_WIDTH / 2 - (offset + 3) * (CELL_WIDTH +
+      // CELL_PAD),
+      //  .to = w / 2 - CELL_WIDTH / 2 - (offset + 3) * (CELL_WIDTH + CELL_PAD),
+      //  .duration = 0.5},
+      // {.from = w / 2 - CELL_WIDTH / 2 - (offset + 0) * (CELL_WIDTH +
+      // CELL_PAD),
+      //  .to = w / 2 - CELL_WIDTH / 2 - (offset + 3) * (CELL_WIDTH + CELL_PAD),
+      //  .duration = 0.5},
   };
+  size_t kfs_count = NOB_ARRAY_LEN(kfs);
 
-  float w = GetScreenWidth();
-  float h = GetScreenHeight();
+  animation_update(a, dt, kfs, kfs_count);
 
-  BeginDrawing();
-  animation_update(&p->a, kfs, NOB_ARRAY_LEN(kfs));
+  Vector2 cell_size = {CELL_WIDTH, CELL_HEIGHT};
+  float t = animation_value(*a, kfs, kfs_count);
 
-  float t = animation_value(p->a, kfs, NOB_ARRAY_LEN(kfs));
+#if 0
+  Color cell_color = ColorFromHSV(0, 0.0, 0.15);
+  Color head_color = ColorFromHSV(0, 0.8, 0.8);
+  Color background_color = ColorFromHSV(120, 0.0, 0.95);
+#else
+  Color cell_color = ColorFromHSV(0, 0.0, 0.85);
+  Color head_color = ColorFromHSV(0, 0.8, 0.8);
+  Color background_color = ColorFromHSV(120, 0.0, 0.05);
+#endif
 
-  ClearBackground(GetColor(0x181818FF));
-
-  float rw = 100.0;
-  float rh = 100.0;
-  float pad = rw * 0.15;
-  Color cell_color = ColorFromHSV(0, 0.8, 1);
+  ClearBackground(background_color);
 
   for (size_t i = 0; i < 20; ++i) { // + GetMousePosition().x
-    DrawRectangle(i * (rw + pad) - w * t, h / 2 - rh / 2, rw, rh, cell_color);
+    Rectangle rec = {
+        .x = i * (CELL_WIDTH + CELL_PAD) + t,
+        .y = h / 2 - CELL_HEIGHT / 2,
+        .width = CELL_WIDTH,
+        .height = CELL_HEIGHT,
+    };
+    DrawRectangleRec(rec, cell_color);
+
+    const char *text = "hi";
+    Vector2 text_size = MeasureTextEx(p->font, text, FONT_SIZE, 0);
+    Vector2 position = {.x = rec.x, .y = rec.y};
+    position = Vector2Add(position, Vector2Scale(cell_size, 0.5));
+    position = Vector2Subtract(position, Vector2Scale(text_size, 0.5));
+    DrawTextEx(p->font, text, position, FONT_SIZE, 0, background_color);
   }
 
   float head_thick = 20.0;
   Rectangle rec = {
-      .width = rw + head_thick * 3,
-      .height = rh + head_thick * 3,
+      .width = CELL_WIDTH + head_thick * 3,
+      .height = CELL_HEIGHT + head_thick * 3,
   };
   rec.x = w / 2 - rec.width / 2;
   rec.y = h / 2 - rec.height / 2;
-  DrawRectangleLinesEx(rec, head_thick, YELLOW);
+  DrawRectangleLinesEx(rec, head_thick, head_color);
+}
 
+void plug_update(void) {
+  BeginDrawing();
+  turing_machine_tape(&p->a, GetFrameTime(), GetScreenWidth(),
+                      GetScreenHeight());
   EndDrawing();
 }
